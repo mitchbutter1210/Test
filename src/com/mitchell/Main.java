53cbc7a8f7daa1c8548207f1ad4a343f572c6c62
mitchbutter1210 <mitchbutter1210@gmail.com>
2014-03-24 23:20:53 -0400

    Cleaned up redundant code with while loop

--- src/com/mitchell/Main.java
+++ src/com/mitchell/Main.java
@@ -6,17 +6,17 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
         String name;
+        int i = 0;
         System.out.println("Welcome to the swag machine!");
         Scanner s = new Scanner(System.in);
         System.out.print("Enter yo name nigga: ");
         name = s.nextLine();
         System.out.println("Hold up my nigga, we're generating your swag name");
-        System.out.println("/");
-        Thread.sleep(1000);
-        System.out.println("/");
-        Thread.sleep(1000);
-        System.out.println("/");
-        Thread.sleep(1000);
+        while(i <= 4){
+            i++;
+            System.out.println("///");
+            Thread.sleep(1000);
+        }
         System.out.println("It appears that your swag name would be: " + name + "swag");
     }
 }
