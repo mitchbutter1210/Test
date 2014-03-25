f848775183447c7e2190f32c1f7e91ece8e03d0b
mitchbutter1210 <mitchbutter1210@gmail.com>
2014-03-24 23:14:10 -0400

    Added basic functionality

--- src/com/mitchell/Main.java
+++ src/com/mitchell/Main.java
@@ -1,8 +1,22 @@
 package com.mitchell;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws InterruptedException {
+        String name;
+        System.out.println("Welcome to the swag machine!");
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter yo name nigga");
+        name = s.nextLine();
+        System.out.println("Hold up my nigga, we're generating your swag name");
+        System.out.println("/");
+        Thread.sleep(1000);
+        System.out.println("/");
+        Thread.sleep(1000);
+        System.out.println("/");
+        Thread.sleep(1000);
+        System.out.println("It appears that your swag name would be: " + name + "swag");
     }
 }
