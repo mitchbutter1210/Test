bc74451ecd816a83d7f3a37438172a422944180c
mitchbutter1210 <mitchbutter1210@gmail.com>
2014-03-25 00:55:16 -0400

    Removed racism

--- src/main/Input.java
+++ src/main/Input.java
@@ -9,9 +9,9 @@ public class Input {
         int i = 0;
         System.out.println("Welcome to the swag machine!");
         Scanner s = new Scanner(System.in);
-        System.out.print("Enter yo name nigga: ");
+        System.out.print("Enter yo name man: ");
         name = s.nextLine();
-        System.out.println("Hold up my nigga, we're generating your swag name");
+        System.out.println("Hold up my awesome man, we're generating your swag name");
         while(i <= 4){
             i++;
             System.out.println("///");
