d276c1bc0ffb17b28241d7854c7990613435ffd0
mitchbutter1210 <mitchbutter1210@gmail.com>
2014-03-31 20:26:25 -0400

    Added minimumOfThree class

--- src/com/codeabbey/minimumOfThree.java
+++ src/com/codeabbey/minimumOfThree.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 /**
  * Created by Mitchell on 3/30/2014.
  */
-public class minimumOfTwo {
+public class minimumOfThree {
 
     public static void main(String[] args){
         int i = 0, z = 0;
@@ -14,10 +14,11 @@ public class minimumOfTwo {
         boolean doneChecking = false;
         int[][] integers;
         int n = s.nextInt();
-        integers = new int[n][2];
+        integers = new int[n][3];
         while(done == false){
             integers[i][0] = s.nextInt();
             integers[i][1] = s.nextInt();
+            integers[i][2] = s.nextInt();
             i++;
             if(i == n){
                 done = true;
@@ -25,16 +26,11 @@ public class minimumOfTwo {
         }
 
         while(doneChecking == false){
-            if(integers[z][0] > integers[z][1]){
-                System.out.print(integers[z][1] + " ");
-                z++;
-            }
+            int minimumFirst = Math.min(integers[z][0], integers[z][1]);
+            int minimumSecond = Math.min(minimumFirst, integers[z][2]);
+            System.out.print(minimumSecond + " ");
 
-            else{
-                System.out.print(integers[z][0] + " ");
-                z++;
-            }
-
+            z++;
             if(z == n){
                 doneChecking = true;
             }
